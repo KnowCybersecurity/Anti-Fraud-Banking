5d5bd0e6b80d0d0be1e3ddf5d03645ef580a8966
KnowCybersecurity <121370426+KnowCybersecurity@users.noreply.github.com>
2022-12-25 01:09:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,91 @@
+function BlockCard(){
+    let Confirmation = prompt("Do you want to block all transactions on this card? ", "Y or N");
+    if (Confirmation.toUpperCase() == "Y"){
+        Status = "CARD BLOCKED";
+        document.getElementById("CardNumber").innerText = Status;
+    }
+}
+
+function UnblockCard(){
+    let Confirmation = prompt("Do you want to unblock all transactions on this card? ", "Y or N");
+    if (Confirmation.toUpperCase() == "Y"){
+        Status = "ACTIVE";
+        RandomCardNumber();
+        TimeLeft = 8;
+    }
+}
+
+function GenerateNewCardNumber(){
+    let Confirmation = prompt("Do you want to generate a new card number for this card? ", "Y or N");
+        if (Confirmation.toUpperCase() == "Y"){
+        TimeLeft = 8;
+        RandomCardNumber();
+    }
+}
+
+function GenerateNewCVV(){
+    let Confirmation = prompt("Do you want to generate a new CVV for this card? ", "Y or N");
+    if (Confirmation.toUpperCase() == "Y"){
+        CVV = RandomCVV();
+        ExpCVV(Exp, CVV);
+    }
+}
+
+
 function RandomCardNumber(){
-
     let CardNumber = "";
-
     for (let i = 0; i < 16; i++){
         let RandomDigit = Math.floor(Math.random() * (DIGITS.length));
         CardNumber += DIGITS[RandomDigit];
-
         if (i == 3 || i == 7 || i == 11 || i == 15){
             CardNumber += " ";
         }
-
     }
-
     document.getElementById("CardNumber").innerText = CardNumber;
     return CardNumber;
-
 }
 
 function RandomExpirationDate(){
-
     let ExpirationDate = "";
-
     let Month = Math.floor(Math.random() * (MONTHS.length));
     ExpirationDate += `${MONTHS[Month]}`;
-
     ExpirationDate += "/";
-
     let Year = Math.floor(Math.random() * (EXP_YEAR.length));
     ExpirationDate += `${EXP_YEAR[Year]}`;
-
     return ExpirationDate;
-
 }
 
-function RandomCVC(){
-
+function RandomCVV(){
     let CVC = "";
-
     for (let i = 0; i < 3; i++){
         let RandomDigit = Math.floor(Math.random() * (DIGITS.length));
         CVC += DIGITS[RandomDigit];
 
     }
-
     return CVC;
-
 }
 
-function ExpCVC(P1_Exp, P2_CVC){
-
-    document.getElementById("ExpirationDate_CVC").innerText = `${P1_Exp} ${P2_CVC}`;
-
-
+function ExpCVV(P1_Exp, P2_CVV){
+    document.getElementById("ExpirationDate_CVC").innerText = `${P1_Exp} ${P2_CVV}`;
 }
+
+var Status = "ACTIVE";
 
 const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const MONTHS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
 const EXP_YEAR = ["25", "30", "35", "40", "45", "50"];
 
 let Exp = RandomExpirationDate();
-let CVC = RandomCVC();
+let CVV = RandomCVV();
 
 let TimeLeft = 9;
 let Interval = setInterval(function(){
 
     if (TimeLeft == 0) {
         TimeLeft = 10;
-        RandomCardNumber();
+        if (Status == "ACTIVE"){
+            RandomCardNumber();
+        }
     } 
 
     document.getElementById("Timer").innerText = `${TimeLeft} seconds until a new card number is generated`;
@@ -75,7 +94,7 @@ let Interval = setInterval(function(){
 
 do {
     RandomCardNumber();
-    ExpCVC(Exp, CVC);
+    ExpCVV(Exp, CVV);
 } while (True){
     setInterval();
 }
